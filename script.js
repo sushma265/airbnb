ab5e14afd44f79cd298a51d63b9fd3d487a5de6c
Sushma <202810092+sushma265@users.noreply.github.com>
2026-09-23 11:25:55 +0530

    added files

---
+++ script.js
@@ -0,0 +1,15 @@
+function loginAlert() {
+    alert("Welcome! Login page will open soon.");
+}
+
+function searchAlert() {
+    alert("Searching for available stays!");
+}
+
+function bookingAlert() {
+    alert("You selected this stay!");
+}
+
+function contactAlert() {
+    alert("Contact us at: support@airbnb.com");
+}
